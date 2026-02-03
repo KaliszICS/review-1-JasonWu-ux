8194b7dbe5c04d021bb0b69695aecfb25f85fab7
JasonWu-ux <jasonwu544@gmail.com>
2026-02-03 15:35:45 +0000

    Finished Questions

--- src/main/java/Review1.java
+++ src/main/java/Review1.java
@@ -1,23 +1,36 @@
-public class Review1 {
+public class Review1{
 
 	public static void q1() {
 		//Write question 1 code here
+		System.out.println("There once was a man from St. Ives.");
 	}
 
 	public static void q2() {
 		//Write question 2 code here
+		int a = 9;
+		int b = 5;
+
+		System.out.println(a / b);
+		System.out.println(a % b);
 	}
 
 	public static void q3() {
 		//Write question 3 code here
+		System.out.println("Hello\nHello again");
 	}
 
 	public static void q4() {
 		//Write question 4 code here
+		boolean truefalse = true;
+		System.out.println(truefalse);
 	}
 
 	public static void q5() {
 		//Write question 5 code here
+		String str1 = "Hello";
+		String str2 = "World";
+
+		System.out.println(str1 + " " + str2);
 	}
 
 }
